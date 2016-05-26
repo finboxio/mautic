776064e494b5d0a0ccb27b921acc98a0b998e323
Brian Dentino <brian.dentino@gmail.com>
2016-05-25 22:33:06 -0700

    [wip] Amazon mailer callbacks

--- app/bundles/EmailBundle/Swiftmailer/Transport/AmazonTransport.php
+++ app/bundles/EmailBundle/Swiftmailer/Transport/AmazonTransport.php
@@ -9,10 +9,14 @@
 
 namespace Mautic\EmailBundle\Swiftmailer\Transport;
 
+use Mautic\CoreBundle\Factory\MauticFactory;
+use Mautic\EmailBundle\Helper\MailHelper;
+use Symfony\Component\HttpFoundation\Request;
+
 /**
  * Class AmazonTransport
  */
-class AmazonTransport extends \Swift_SmtpTransport
+class AmazonTransport extends \Swift_SmtpTransport implements InterfaceCallbackTransport
 {
     /**
      * {@inheritdoc}
@@ -22,4 +26,59 @@ class AmazonTransport extends \Swift_SmtpTransport
         parent::__construct($host, 587, 'tls');
         $this->setAuthMode('login');
     }
+
+    /**
+     * Returns a "transport" string to match the URL path /mailer/{transport}/callback
+     *
+     * @return mixed
+     */
+    public function getCallbackPath()
+    {
+        return 'amazon';
+    }
+
+    /**
+     * Handle response
+     *
+     * @param Request       $request
+     * @param MauticFactory $factory
+     *
+     * @return mixed
+     */
+    public function handleCallbackResponse(Request $request, MauticFactory $factory)
+    {
+        $msg_type1 = $request->headers->get('X-AMZ-SNS-MESSAGE-TYPE')
+        $msg_type2 = $request->headers->get('x-amz-sns-message-type')
+
+        $logger = $this->factory->getLogger()
+        $logger->error("Received message type $msg_type1 or $msg_type2")
+
+        // if (is_array($mandrillEvents)) {
+        //     foreach ($mandrillEvents as $event) {
+        //         $isBounce      = in_array($event['event'], array('hard_bounce', 'soft_bounce', 'reject', 'spam', 'invalid'));
+        //         $isUnsubscribe = ('unsub' === $event['event']);
+        //         if ($isBounce || $isUnsubscribe) {
+        //             $type = ($isBounce) ? 'bounced' : 'unsubscribed';
+
+        //             if (!empty($event['msg']['diag'])) {
+        //                 $reason = $event['msg']['diag'];
+        //             } elseif (!empty($event['msg']['bounce_description'])) {
+        //                 $reason = $event['msg']['bounce_description'];
+        //             } else {
+        //                 $reason = ($isUnsubscribe) ? 'unsubscribed' : $event['event'];
+        //             }
+
+        //             if (isset($event['msg']['metadata']['hashId'])) {
+        //                 $rows[$type]['hashIds'][$event['msg']['metadata']['hashId']] = $reason;
+        //             } else {
+        //                 $rows[$type]['emails'][$event['msg']['email']] = $reason;
+        //             }
+        //         }
+        //     }
+        // }
+
+        return array()
+        //$rows;
+    }
 }
+
